a00f95b68397d5edabbecb31d663f60dce9aac6d
McBain <perlmagik@gmail.com>
2025-01-02 18:15:42 +0000

    Update perl_proxy_methods.pl

--- perl_proxy_methods.pl
+++ perl_proxy_methods.pl
@@ -1,22 +1,25 @@
-Configuring LWP::UserAgent to Use a Proxy
-LWP::UserAgent is the core component in Perl for making web requests. Here's how to point it at a proxy:
+#############################################
+# Configuring LWP::UserAgent to Use a Proxy #
+#############################################
 
-1. Use Environment Variables
-Set the HTTP_PROXY or HTTPS_PROXY env vars to your proxy URL:
+#######################################################################################################
+# Method 1 - use environment variables - set the HTTP_PROXY or HTTPS_PROXY env vars to your proxy URL #
+#######################################################################################################
 
-Copy
 $ENV{HTTP_PROXY} = '<http://192.168.1.42:8080>';
+
 $ua = LWP::UserAgent->new;
 $ua->env_proxy;
-This automatically picks up the proxy from the environment.
 
-2. The proxy() Method
-You can directly pass the proxy to use through proxy():
+# This automatically picks up the proxy from the environment
 
-Copy
+###################################################################################
+# Method 2 - The proxy () method - directly pass the proxy to use through proxy() #
+###################################################################################
+
 my $ua = LWP::UserAgent->new;
 $ua->proxy('http', '<http://proxy.example.com:8080>');
-Also set protocols_allowed to enforce using the proxy:
 
-Copy
+# set protocols_allowed to enforce using the proxy:
+
 $ua->protocols_allowed(['http']);
