95e704dae2e9190ec4fd6f1e170389dcce6199f7
McBain <perlmagik@gmail.com>
2026-05-06 18:22:29 +0100

    Update perl_mechanize_for_extracting_links_examined.pl

--- internet_of_things/perl_mechanize_for_extracting_links_examined.pl
+++ internet_of_things/perl_mechanize_for_extracting_links_examined.pl
@@ -14,8 +14,33 @@
 
 # can redirect the print output to a file from the command line and then use wget to download
 
+###################################################################
+# the difference between $mech->images and $mech->find_all_images #
+####################################################################
 
+# In WWW::Mechanize, both methods deal with images on the current page, but they behave quite differently:
+$mech->images
+    • Returns a list of image objects (specifically WWW::Mechanize::Image objects). 
+    • By default, it only includes images that have a src attribute. 
+    • It’s a simpler, older interface meant for quick access. 
+# $mech->find_all_images
+    • Returns a list of image objects matching criteria you specify. 
+    • Accepts filters like tag, src_regex, alt, etc. 
+    • More flexible and powerful—designed for searching and filtering images.
+# In short:
+    • images = “give me the basic list of images” 
+    • find_all_images = “search for images that match these conditions” 
+	
+# If you need fine-grained control (e.g., only images with certain URLs or alt text), find_all_images is the better choice.
+# You don’t have to pass anything to find_all_images - it works fine with no parameters
 
+# Calling:
+# my @imgs = $mech->find_all_images();
+# will return all images on the page, much like $mech->images.
+
+# So what’s the difference in that case?
+    • With no arguments, find_all_images behaves very similarly to images. 
+    • The key advantage is that find_all_images can take filters when you need them, while images cannot. 
 
 #!/usr/bin/perl
 use strict;
@@ -53,7 +78,6 @@ foreach my $link ($mech->links) {
 # check internal and external image urls #
 ##########################################
 
-
 foreach my $img ($mech->images) {
 	my $url  = URI->new_abs($img->url, $mech->uri);
         my $host = $url->host;
@@ -64,5 +88,6 @@ foreach my $img ($mech->images) {
          else {
             print "External image: $url\n";
         }
+exit;
 }
 exit;
