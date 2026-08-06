3f901ad26692310ad89014890926a9fba80ebb62
McBain <cwma.campbell@gmail.com>
2026-08-06 11:00:27 +0100

    Add files via upload

--- onetrust_asset_vendor_project.pl
+++ onetrust_asset_vendor_project.pl
@@ -4,7 +4,7 @@
 # module AssetLookup reads asset status file from OneTrust - filtered for Active
 # active projects is filtered in this script
 # why this setup?
-# need active projects - the SDE Project Log.txt file provides this info
+# need active projects - the SDE Project Log Perl.txt file provides this info
 # need active users - the SDE Project Log.txt file does not provide this info
 # need active assets - the SDE Project Log.txt file does not provide this info
 # so... VendorLookup reads vendor status.txt and filters on active vendors
@@ -42,7 +42,7 @@ my $file_date = sprintf(
 # usage:
 #   perl script.pl input_file.txt
 
-my $file = shift @ARGV or die "Usage: $0 SDE Project Log.txt needed... download from OneTrust\n";
+my $file = shift @ARGV or die "Usage: $0 SDE Project Log Perl.txt needed... download from OneTrust\n";
 
 open (my $fh, '<', $file) or die "Cannot open $file: $!\n";
 
@@ -72,7 +72,7 @@ for my $i (0 .. $#headers) {
 }
 
 # Check required columns exist - name in list is checked as a hash key
-for my $required ('SDE Project ID', 'Name - Related (Vendors)', 'Name - Related (Assets)', 'SDE Project Status') {
+for my $required ('SDE Project ID', 'Name', 'Name - Related (Vendors)', 'Name - Related (Assets)', 'SDE Project Status', 'Status', 'SDE Project Start Date', 'SDE Project End Date') {
     die "Missing column: $required\n"
         unless exists $col{$required};
 }
@@ -87,38 +87,49 @@ while (<$fh>) {
     # remove carraige returns
     s/\r$//;
 
-    # preserve empty trailing columns
+    # preserve empty trailing column with -1
     my @fields = split /\t/, $_, -1;
 
-    # do not inlcude archived projects
-    my $status = $fields[$col{'SDE Project Status'}];
-    next if $status ne 'Active';
-
-    # extract values
-    my $project = $fields[$col{'SDE Project ID'}] // '';
-    my $vendors = $fields[$col{'Name - Related (Vendors)'}] // '';
-    my $assets = $fields[$col{'Name - Related (Assets)'}] // '';
+     my $project = $fields[$col{'SDE Project ID'}];
+     my $name    = $fields[$col{'Name'}];
+     my $vendors = $fields[$col{'Name - Related (Vendors)'}];
+     my $assets  = $fields[$col{'Name - Related (Assets)'}];
+     my $SDE_status  = $fields[$col{'SDE Project Status'}];
+     my $status      = $fields[$col{'Status'}];
+     my $start_date  = $fields[$col{'SDE Project Start Date'}];
+     my $finish_date = $fields[$col{'SDE Project End Date'}];
 
     # remove surrounding quotes
     $vendors =~ s/^"(.*)"$/$1/;
     $assets  =~ s/^"(.*)"$/$1/;
 
-    # if no project - create a no project value
-    if (!$project) {
-        $project = '[No Project ID]';
-    }
+    # populate undefined or empty values
+    # vendor NA is a key... needs to be unique - therefore line number inlcuded to make unique
+	
+    $SDE_status  = 'no SDE status' if !defined($SDE_status) || $SDE_status eq '';
+    $status  = 'no status' if !defined($status) || $status eq '';
+    $vendors = "line $. in SDE Project Log - No vendor" if !defined($vendors) || $vendors eq '';
+    $project = 'no project in SDE Project Log' if !defined($project) || $project eq '';
+    $assets  = 'no asset in SDE Project Log' if !defined($assets)  || $assets eq '';
+    $name    = 
+    $start_date  =
+    $finish_date =
 
+    # do not inlcude archived projects
+    # if $SDE_Status or $Status is undefined or an empty string - skip
+    next if $SDE_status eq 'Archived' || $status eq 'Archived';
+
+    print "$name\t$vendors\t$assets\t$SDE_status\t$status\t$start_date\t$finish_date\n";
+
+ # }
+   # get to the vendors which are a string - with comma separated delimiter for each vendor
     # split vendors
     my @vendor_list = split /\s*,\s*/, $vendors;
 
+    # get to the assets which are a string - with comma separated delimiter for each asset
     # split assets
     my @asset_list = split /\s*,\s*/, $assets;
-
-    # if no asset - create a no asset value
-    if (!@asset_list || !$asset_list[0]) {
-        @asset_list = ('[No Asset Listed]');
-    }
-
+  
     # data structure
 
     # vendor
@@ -128,23 +139,22 @@ while (<$fh>) {
     #           +-- { project => ID }
 
 for my $vendor (@vendor_list) {
-    next unless $vendor;
-
-    # Normalize vendor name
+    # normalize vendor name
     $vendor =~ s/\([^)]*\)//g;
     $vendor =~ s/\s+/ /g;
     $vendor =~ s/^\s+|\s+$//g;
   
         for my $asset (@asset_list) {
-            next unless $asset;
+            # normalize asset name
             $asset =~ s/^\s+|\s+$//g;
 
             # Prevent duplicate vendor/asset/project
             my $unique_key = join('|', $vendor, $asset, $project);
             next if $seen{$unique_key};
 
-            # store project as an array element
-            # project - hash key, $project - hash value
+            # store project and vendor as hash key and value inside an array
+            # vendor = outer hash key
+            # project - hash key, $asset - hash value inside an array
             push @{ $vendor_data{$vendor}{$asset} },
                 {
                     project => $project
@@ -160,7 +170,7 @@ close $fh;
 # only archived projects filtered out at this stage from main script
 # archived assets and vendors inlcuded
 # check structure
-# print Dumper(\%data);
+# print Dumper(\%vendor_data);
 
 open (my $dd,'>','OneTrust_vendor_asset_project_json_map.json');
 my $json = JSON->new->pretty(1);
@@ -174,9 +184,9 @@ my $hash_vendor = read_vendor_file("vendor status.txt");
 my $hash_asset  = read_asset_file("asset status.txt");
 
 # check that only active assets and vendors are included for new data structures
-print Dumper ($hash_asset);
-print "\n";
-print Dumper ($hash_vendor);
+# print Dumper ($hash_asset);
+# print "\n";
+# print Dumper ($hash_vendor);
 
 # create the data team structure so can be filtered out
 my %exclude_vendor = map { $_ => 1 } (
@@ -219,17 +229,25 @@ for my $vendor (sort keys %vendor_data) {
 }
 
 # open the files for outputs
-open (my $vendor_out,  '>', "vendor_output_$file_date.txt") or die $!;
-open (my $project_out, '>', "project_output_$file_date.txt") or die $!;
-open (my $asset_out,   '>', "asset_output_$file_date.txt") or die $!;
+# open (my $vendor_out,  '>', "vendor_output_$file_date.txt") or die $!;
+# open (my $project_out, '>', "project_output_$file_date.txt") or die $!;
+# open (my $asset_out,   '>', "asset_output_$file_date.txt") or die $!;
 
 # the subroutines and data structures to file outputs
-print_vendor_first(\%vendor_data_filtered, $vendor_out);
-print_project_first(\%project_data, $project_out);
-print_asset_first(\%asset_data, $asset_out);
+# print_vendor_first(\%vendor_data_filtered, $vendor_out);
+# print_project_first(\%project_data, $project_out);
+# print_asset_first(\%asset_data, $asset_out);
 
-close $vendor_out;
-close $project_out;
-close $asset_out;
+# close $vendor_out;
+# close $project_out;
+# close $asset_out;
+
+# test stuff
+#print "vendor" , "\n";
+#print Dumper(\%vendor_data_filtered) , "\n";
+#print "project" , "\n";
+#print Dumper(\%project_data) , "\n";
+#print "asset" , "\n";
+#print Dumper(\%asset_data) , "\n";
 
 exit;
